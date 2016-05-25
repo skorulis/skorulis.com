736d2fbda01fd0a1744c05a5eff4a58a77924d38
Alexander Skorulis <skorulis@gmail.com>
2016-05-25 13:40:41 +1000

    Untapped work

--- beer/scripts/buildExtra.rb
+++ beer/scripts/buildExtra.rb
@@ -1,12 +1,15 @@
 require 'rubygems'
 require 'json'
+require 'net/http'
+require 'httpclient'
+
+ACCESS_TOKEN = "504CD86D79F7BC336C7A36EC0A97655DCF872857"
 
 file = File.read('js/raw.json')
 allBeers = JSON.parse(file)
 existing = JSON.parse(File.read('js/extra.json'))
 
-
-result = Hash.new
+extraData = Hash.new
 
 
 allBeers.each do |item|
@@ -18,11 +21,29 @@ allBeers.each do |item|
 		d["untappd"] = old["untappd"]
 	end
 
+	extraData[item["name"]] = d
+end
 
-	result[item["name"]] = d
+extraData.each do |item|
+	hash = item[1]["untappd"]
+	if hash["id"].length > 0 && hash["style"] == nil
+		url = "https://api.untappd.com/v4/beer/info/" + hash["id"] + "?access_token=" + ACCESS_TOKEN
+		clnt = HTTPClient.new;
+		data = clnt.get_content(url)
+		result = JSON.parse(data)
+		beer = result["response"]["beer"]
+		brewery = beer["brewery"]
+		hash["style"] = beer["beer_style"]
+		hash["IBU"] = beer["beer_ibu"]
+		hash["score"] = beer["rating_score"]
+		hash["brewery"] = brewery["brewery_name"]
+		hash["country"] = brewery["country_name"]
+		
+		break
+	end
 end
 
 File.open("js/extra.json","w") do |f|
-  f.write(JSON.pretty_generate(result))
+  f.write(JSON.pretty_generate(extraData))
 end
 
